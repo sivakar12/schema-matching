b381fe41cdc8a29d795d390327d4df63a056cc39
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-10-03 09:33:08 +0530

    Add log_loss metric

--- schemamatching/metrics.py
+++ schemamatching/metrics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import log_loss
 
 def generate_actual_pairs_dataframe(pairs_file_text):
     pairs = [line.strip().split(':') for line in pairs_file_text.split('\n')]
@@ -16,4 +17,9 @@ def mean_difference(results, actual_results):
     columns = actual_results.columns
     rows = actual_results.index
     diff = results.loc[rows, columns].values - actual_results.values
-    return -np.sum(np.abs(diff)) /( diff.shape[0] * diff.shape[1]) 
+    return -np.sum(np.abs(diff)) /( diff.shape[0] * diff.shape[1])
+
+def average_log_loss(true_mappings, pred_mappings):
+    pred_subset = pred_mappings.loc[true_mappings.index, true_mappings.columns]
+    losses = [log_loss(true_mappings.iloc[i], pred_subset.iloc[i]) for i in range(pred_subset.shape[0])]
+    return sum(losses) / len(losses)
