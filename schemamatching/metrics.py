9e421a29ad1b6e1c9b767970eb943d2f7d0e3d21
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-10-05 21:06:12 +0530

    Add more metric functions

--- schemamatching/metrics.py
+++ schemamatching/metrics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, accuracy_score, f1_score, precision_score, recall_score
 
 def generate_actual_pairs_dataframe(pairs_file_text):
     pairs = [line.strip().split(':') for line in pairs_file_text.split('\n')]
@@ -13,6 +13,7 @@ def generate_actual_pairs_dataframe(pairs_file_text):
         df.fillna(0.0, inplace=True)
     return df
 
+
 def mean_difference(results, actual_results):
     columns = actual_results.columns
     rows = actual_results.index
@@ -20,6 +21,18 @@ def mean_difference(results, actual_results):
     return -np.sum(np.abs(diff)) /( diff.shape[0] * diff.shape[1])
 
 def average_log_loss(true_mappings, pred_mappings):
-    pred_subset = pred_mappings.loc[true_mappings.index, true_mappings.columns]
+    pred_subset = get_subset(true_mappings, pred_mappings)
     losses = [log_loss(true_mappings.iloc[i], pred_subset.iloc[i]) for i in range(pred_subset.shape[0])]
     return sum(losses) / len(losses)
+
+def accuracy(true_mappings, pred_mappings):
+    pred_subset = get_subset(true_mappings, pred_mappings)
+    return accuracy_score(true_mappings.apply(np.argmax), pred_mappings.apply(np.argmax))
+
+def precision(true_mappings, pred_mappings):
+    pred_subset = get_subset(true_mappings, pred_mappings)
+    return precision_score(true_mappings.apply(np.argmax), pred_mappings.apply(np.argmax))
+
+def recall(true_mappings, pred_mappings):
+    pred_subset = get_subset(true_mappings, pred_mappings)
+    return recall_score(true_mappings.apply(np.argmax), pred_mappings.apply(np.argmax))
