3678e1ac632747c0b3b72c5b7faaba57982baffc
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-10-09 20:02:06 +0530

    Make SimilarityFlooding a class and calculate change in scores

--- schemamatching/graph_generation.py
+++ schemamatching/graph_generation.py
@@ -2,17 +2,19 @@ import networkx as nx
 import xml.etree.ElementTree as ET
 
 def generate_graph(xml_string):
-    def _recurse(Graph, node):
-        tag = node.tag
-        if tag not in Graph.nodes:
-            Graph.add_node(tag)
+    def _recurse(Graph, node, parent_path='/'):
+        node_name = parent_path + node.tag
+        if node_name not in Graph.nodes:
+            Graph.add_node(node_name)
         for attribute in node.attrib.keys():
-            if (node.tag, attribute) not in Graph.edges:
-                Graph.add_edge(node.tag, attribute, type='attribute')
+            attribute_name = parent_path[:-1] + '#' + attribute
+            if (node_name, attribute_name) not in Graph.edges:
+                Graph.add_edge(node_name, attribute_name, type='attribute')
         for child in node.getchildren():
-            if (node.tag, child.tag) not in Graph.edges:
-                Graph.add_edge(node.tag, child.tag, type='child')
-            _recurse(Graph, child)
+            child_name = node_name + '/' + child.tag
+            if (node_name, child_name) not in Graph.edges:
+                Graph.add_edge(node_name, child_name, type='child')
+            _recurse(Graph, child, parent_path + node.tag + '/')
         
     G = nx.DiGraph()
     xml_tree = ET.fromstring(xml_string)
@@ -49,5 +51,30 @@ def do_one_interation_of_flooding(G):
     
     for node, data in G2.nodes(data=True):
         G2.nodes[node]['score'] = data['score'] / max_score
-    nx.nodes
     return G2
+
+class SimilarityFlooding:
+    def __init__(self, xml_1, xml_2):
+        self.graph_1 = generate_graph(xml_1)
+        self.graph_2 = generate_graph(xml_2)
+        self.pairwise_graph = generate_pairwise_graph( \
+            self.graph_1, self.graph_2)
+        self.initial_weights =  None
+    
+    def set_initial_scores(self, score_matrix):
+        for row in score_matrix.index:
+            for column in score_matrix.index:
+                self.pairwise_graph.nodes[(row, column)]['score'] = \
+                    score_matrix.loc[row, column]
+    
+    def flood_once(self):
+        """
+        Returns the average of the change of score in each node
+        """
+        initial_scores = {k: v['score'] for k, v in \
+            dict(self.pairwise_graph.nodes(data=True)).items()}
+        self.pairwise_graph = do_one_interation_of_flooding(self.pairwise_graph)
+        final_scores = {k: v['score'] for k, v in \
+            dict(self.pairwise_graph.nodes(data=True)).items()}
+        return sum(abs(initial_scores[key] - final_scores[key]) \
+            for key in initial_scores) / len(initial_scores.values())
