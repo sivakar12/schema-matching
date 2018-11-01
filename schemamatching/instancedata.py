dc084a18dcb3f27dcdbec231ca1aa1ac74e02b35
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-11-01 08:52:38 +0530

    Make it possible to pass custom feature selector

--- schemamatching/instancedata.py
+++ schemamatching/instancedata.py
@@ -8,18 +8,46 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.feature_selection import SelectFromModel
 from .pipeline_components import DFFeatureUnion, DummyTransformer, ColumnExtractor
 
+def collect_instance_data(xml_string):
+    def _recurse(data, current_path, xml_element):
+        current_path = current_path + '/' + xml_element.tag
+        for key in xml_element.attrib:
+            attr_path = current_path + '#' + key
+            if attr_path not in data:
+                data[attr_path] = []
+            data[attr_path].append(xml_element.attrib[key])
+        if len(xml_element) == 0:
+            if current_path not in data:
+                data[current_path] = []
+            data[current_path].append(xml_element.text)
+        else:
+            for child in xml_element:
+                _recurse(data, current_path, child)
+    data = {}
+    xml_tree = ET.fromstring(xml_string)
+    _recurse(data, '', xml_tree)
+    return data
+
+def get_tag_names(xml_string):
+    return collect_instance_data(xml_string).keys()
+
+def get_features(xml):
+    records = []
+    instance_data_dict = collect_instance_data(xml)
+    for tag in instance_data_dict:
+        for instance in instance_data_dict[tag]:
+            records.append((instance, tag))
+    return pd.DataFrame.from_records(records, columns=['content', 'tag'])
+
 def datatype(a):
     try:
         float(a)
         if("." in a):
-            # return "float"
-            return 0
+            return "float"
         else:
-            # return "int"
-            return 1
+            return "int"
     except:
-        # return "string"
-        return 2
+        return "string"
 
 class LengthTransformer(TransformerMixin):
     def fit(self, X, y=None):
@@ -39,7 +67,7 @@ class DataTypeTransformer(TransformerMixin):
         return X_new
         # return X['content'].apply(datatype)
 
-def create_pipeline(classifier, feature_selection=False, length=False, datatype=False):            
+def create_pipeline(classifier, feature_selector=None, length=False, datatype=False):            
     pipeline_items = []
     feature_extractors = []
 
@@ -55,52 +83,21 @@ def create_pipeline(classifier, feature_selection=False, length=False, datatype=
     features = FeatureUnion(feature_extractors)
 
     pipeline_items.append(('features', features))
-    if feature_selection:
-        pipeline_items.append(('feature_selection', SelectFromModel(classifier)))
+    if feature_selector:
+        pipeline_items.append(('feature_selector', feature_selector))
     pipeline_items.append(('classifier', classifier))
     
     pipeline = Pipeline(pipeline_items)
     return pipeline
 
-def collect_instance_data(xml_string):
-    def _recurse(data, current_path, xml_element):
-        current_path = current_path + '/' + xml_element.tag
-        for key in xml_element.attrib:
-            attr_path = current_path + '#' + key
-            if attr_path not in data:
-                data[attr_path] = []
-            data[attr_path].append(xml_element.attrib[key])
-        if len(xml_element) == 0:
-            if current_path not in data:
-                data[current_path] = []
-            data[current_path].append(xml_element.text)
-        else:    
-            for child in xml_element:
-                _recurse(data, current_path, child)
-    data = {}
-    xml_tree = ET.fromstring(xml_string)
-    _recurse(data, '', xml_tree)
-    return data
-
-def get_tag_names(xml_string):
-    return collect_instance_data(xml_string).keys()
-
-def get_features(xml):
-    records = []
-    instance_data_dict = collect_instance_data(xml)
-    for tag in instance_data_dict:
-        for instance in instance_data_dict[tag]:
-            records.append((instance, tag))
-    return pd.DataFrame.from_records(records, columns=['content', 'tag'])
-
 def compare_xmls(xml1, xml2, model=None, \
-        feature_selection=False, length=False, datatype=False):
+        feature_selector=None, length=False, datatype=False):
     xml1_data = collect_instance_data(xml1)
     xml2_data = collect_instance_data(xml2)
     xml2_features = get_features(xml2)
 
     if model == None: model = DecisionTreeClassifier(random_state=42)
-    pipeline = create_pipeline(model, feature_selection, length, datatype)
+    pipeline = create_pipeline(model, feature_selector, length, datatype)
     pipeline.fit(xml2_features['content'], xml2_features['tag'])
 
     output_shape =len(xml1_data.keys()), len(xml2_data.keys())
