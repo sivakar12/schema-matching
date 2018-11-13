d3150b644cdebe516b9dd668ec269f3fab3bf9bf
Sivakar Sithamparanathan <sivakar12@outlook.com>
2018-11-13 14:23:47 +0530

    Use Hungarian algorithm before scoring

--- schemamatching/metrics.py
+++ schemamatching/metrics.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import log_loss as sklearn_log_loss, \
     accuracy_score, f1_score, precision_score, recall_score
+from scipy.optimize import linear_sum_assignment
 
 def make_true_mappings_dataframe(pairs_file_text):
     pairs = [line.strip().split(':') for line in pairs_file_text.split('\n') if line != '']
@@ -23,6 +24,15 @@ def get_intersection(true_mappings, pred_mappings):
     columns = set(true_mappings.columns).intersection(set(pred_mappings.columns))
     return true_mappings.loc[rows, columns], pred_mappings.loc[rows, columns]
 
+def apply_hungarian(matrix):
+    row_indices, col_indices = linear_sum_assignment(-1 * matrix)
+    new_matrix = pd.DataFrame(np.zeros_like(matrix.values), 
+                            index=matrix.index, columns=matrix.columns)
+    for row, col in zip(row_indices, col_indices):
+        new_matrix.iloc[row, col] = 1
+    return new_matrix
+
+
 def mean_difference(true_mappings, pred_mappings):
     true, pred = get_intersection(true_mappings, pred_mappings)
     diff = true.values - pred.values
@@ -33,20 +43,24 @@ def log_loss(true_mappings, pred_mappings):
     return -sklearn_log_loss(np.argmax(true_subset.values, axis=1), pred_subset.values)
 
 def accuracy(true_mappings, pred_mappings):
+    pred_mappings = apply_hungarian(pred_mappings)
     true_subset, pred_subset = get_intersection(true_mappings, pred_mappings)
     return accuracy_score(true_subset.idxmax(), pred_subset.idxmax())
 
 def precision(true_mappings, pred_mappings):
+    pred_mappings = apply_hungarian(pred_mappings)
     true_subset, pred_subset = get_intersection(true_mappings, pred_mappings)
-    return precision_score(true_subset.idxmax(), pred_subset.idxmax(), average='weighted')
+    return precision_score(true_subset.idxmax(), pred_subset.idxmax(), average='micro')
 
 def recall(true_mappings, pred_mappings):
+    pred_mappings = apply_hungarian(pred_mappings)
     true_subset, pred_subset = get_intersection(true_mappings, pred_mappings)
-    return recall_score(true_subset.idxmax(), pred_subset.idxmax(), average='weighted')
+    return recall_score(true_subset.idxmax(), pred_subset.idxmax(), average='micro')
 
 def f1(true_mappings, pred_mappings):
+    pred_mappings = apply_hungarian(pred_mappings)
     true_subset, pred_subset = get_intersection(true_mappings, pred_mappings)
-    return f1_score(true_subset.idxmax(), pred_subset.idxmax(), average='weighted')
+    return f1_score(true_subset.idxmax(), pred_subset.idxmax(), average='micro')
 
 def print_all_scores(true_mappings, pred_mappings):
     print("Mean difference: ", mean_difference(true_mappings, pred_mappings))
